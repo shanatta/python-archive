46512881313565084885c6f14aafaccf6a8e22e1
Shanatta <114990082+shanatta@users.noreply.github.com>
2023-04-05 19:01:05 +0700

    Create variabel.py

---
+++ src/variabel.py
@@ -0,0 +1,24 @@
+#Variabel
+
+#-penamaan variabel
+nilai_a = 15 #underscore
+juta2 = 2000000 #angka tidak boleh di depan
+nilaiB = 29.5 #camel case
+
+a = 28 #bisa ubah variabel
+panjang = 20 
+lebar = 8
+
+#-pemanggilan pertama
+print("Nilai a=", a)
+print("Panjang=", panjang)
+print("Lebar=", lebar)
+
+#-pemanggilan kedua (bisa dipanggil lagi)
+print("Nilai a=", a)
+a = 12 #bisa ubah variabel
+print("Nilai a", a) 
+
+#assignment indirect
+b = a 
+print("Nilai b", b) 
