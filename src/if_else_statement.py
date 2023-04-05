bbbeb932d197da789ae7be809f36ca21bb8c9429
Shanatta <114990082+shanatta@users.noreply.github.com>
2023-04-05 18:44:45 +0700

    Create if_else_statement.py

---
+++ src/if_else_statement.py
@@ -0,0 +1,22 @@
+#If Else Statement
+
+#-if inline
+print("==============If Inline==============")
+nama = input("Nama: ")
+if nama=="azril" : print("-Lop yuu!!")
+
+#-if indentation
+print("===========If Indentation===========")
+nama = input("Nama: ")
+if nama=="baihaqy" : 
+    print("-Sama aja")
+    print("-Lop yuu cinaa") 
+
+#-else statement
+print("===========Else Statement===========")
+nama = input("Nama: ")
+if nama=="nata" : 
+    print("-Kamu ga cina")
+else:
+    print("-Kamu bukan nata") 
+    
